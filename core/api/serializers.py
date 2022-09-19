ae0545a2ce2643a47e342384cf03855ba304a4b6
iMaksus <max1112vas@gmail.com>
2022-09-19 17:02:04 +0300

    Added test business-logic
    Added business-logic for testing virtual environment

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import *
+
+class CustomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['pk', 'name', 'email', 'created']
--- core/api/views.py
+++ core/api/views.py
@@ -2,3 +2,28 @@ from django.shortcuts import render
 from .models import *
 from rest_framework import generics
 from .serializers import *
+
+class CustomerCreate(generics.CreateAPIView):
+    # API endpoint that allows creation of a new customer
+    queryset = Customer.objects.all(),
+    serializer_class = CustomSerializer
+
+class CustomerList(generics.ListAPIView):
+    # API endpoint that allows customer to be viewed
+    queryset = Customer.objects.all()
+    serializer_class = CustomSerializer
+
+class CustomerDetail(generics.RetrieveAPIView):
+    # API endpoint that returns a single customer by pk
+    queryset = Customer.objects.all()
+    serializer_class = CustomSerializer
+
+class CustomerUpdate(generics.RetrieveUpdateAPIView):
+    # API endpoint that allows a customer record to be updated
+    queryset = Customer.objects.all()
+    serializer_class = CustomSerializer
+
+class CustomerDelete(generics.RetrieveDestroyAPIView):
+    # API endpoint that allows a customer record to be updated
+    queryset = Customer.objects.all()
+    serializer_class = CustomSerializer
